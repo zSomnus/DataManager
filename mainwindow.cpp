39076f862f5830706fdea4a06dcaddba86cd8b6a
Ella <ellazhang37@gmail.com>
2021-02-03 06:43:00 -0400

    Remove select row

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,7 +12,6 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     mModel = new QStandardItemModel(this);
     ui->tableView->setModel(mModel);
-    ui->tableView->setSelectionBehavior(QAbstractItemView::SelectRows);
     setCentralWidget(ui->tableView);
     setWindowTitle("Data Manager");
 
